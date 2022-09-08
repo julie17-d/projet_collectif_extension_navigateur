7b1b7f731444fa50d0987318594b56841b71ac0f
Greenhall <christophergreenhall@gmail.com>
2022-09-08 16:03:42 +0200

    added extension input and enabled launch button to open article in new tab

--- FOMO/notifications.js
+++ FOMO/notifications.js
@@ -1,32 +1,38 @@
 function createAnImageNotif(title, description, date, imageUrl) {
-    chrome.notifications.create("NOTIFICATION_ID", {
-        type: "image",
-        iconUrl: "/images/icon.png",
-        imageUrl: imageUrl,
-        title: title,
-        message: description,
-        priority: 2,
-        buttons: [
-            {
-                title: "read now"
-            }
-        ],
-        eventTime: date
-    })
+  chrome.notifications.create("01", {
+    type: "image",
+    iconUrl: "/images/icon.png",
+    imageUrl: imageUrl,
+    title: title,
+    message: description,
+    priority: 2,
+    buttons: [
+      {
+        title: "read now",
+      },
+    ],
+    eventTime: date,
+  });
 }
 
 function createABasicNotif(title, description, date) {
-    chrome.notifications.create("", {
-        type: "basic",
-        iconUrl: "/images/icon.png",
-        title: title,
-        message: description,
-        priority: 2,
-        buttons: [
-            {
-                title: "read now"
-            }
-        ],
-        eventTime: date
-    })
+  chrome.notifications.create("01", {
+    type: "basic",
+    iconUrl: "/images/icon.png",
+    title: title,
+    message: description,
+    priority: 2,
+    buttons: [
+      {
+        title: "Read now",
+      },
+    ],
+    eventTime: date,
+  });
+}
+
+function readNow(link) {
+  chrome.notifications.onButtonClicked.addListener(function (tab) {
+    chrome.tabs.create({ url: link });
+  });
 }
--- FOMO/popup.js
+++ FOMO/popup.js
@@ -5,30 +5,26 @@ let userTopic;
 let url;
 
 function buttonUpdate() {
-//store the data related to topic
+  //store the data related to topic
   let topic = document.getElementById("topic");
   userTopic = topic.value;
-
-//format the topic without spaces
-  // userTopic = userTopic.split(" ");
-  // userTopic = userTopic.join("-");
   console.log(userTopic);
 
-//store the data related to date
-//   let date = document.getElementById("date");
-//   let userDate = date.value;
-//   date = new Date();
+  //store the data related to date
+  //   let date = document.getElementById("date");
+  //   let userDate = date.value;
+  //   date = new Date();
 
-// //format the date from string to object
-//   if(userDate === "yesterday"){
-//     date.setDate(date.getDate() - 1);
-//   } else if(userDate === "three days ago"){
-//     date.setDate(date.getDate() - 3);
-//   } else if(userDate === "a week ago"){
-//     date.setDate(date.getDate() - 7);
-//   }
+  // //format the date from string to object
+  //   if(userDate === "yesterday"){
+  //     date.setDate(date.getDate() - 1);
+  //   } else if(userDate === "three days ago"){
+  //     date.setDate(date.getDate() - 3);
+  //   } else if(userDate === "a week ago"){
+  //     date.setDate(date.getDate() - 7);
+  //   }
 
-//extract infos from date and format for API
+  //extract infos from date and format for API
   // let year = String(date.getFullYear());
   // let month = String(date.getMonth() + 1);
   // if (month < 10) {
@@ -44,7 +40,9 @@ function buttonUpdate() {
 
   //let url = 'https://newsdata.io/api/1/news?apikey=pub_11024b51bab49e5c0e5be61de5f0eb348afdb&language=fr,en&q=' + 'Apple' + '&from=' + fullDate + '&apiKey=f36578cbd49a4d378930151f1e3ac0d6';
 
-  url = 'https://newsdata.io/api/1/news?apikey=pub_11024b51bab49e5c0e5be61de5f0eb348afdb&language=fr,en&q=' + userTopic;
+  url =
+    "https://newsdata.io/api/1/news?apikey=pub_1096463a00f38152b895f422ed9db51349ba9&language=fr,en&q=" +
+    userTopic;
 
   fetchArticles();
 }
@@ -53,34 +51,36 @@ function buttonUpdate() {
 async function fetchArticles() {
   const response = await fetch(url);
   const data = await response.json();
-  //return await data.articles[0];   
   console.log(data.results[0].title);
   let title = data.results[0].title;
   let link = data.results[0].link;
-  let pubDate = parseInt(data.results[0].pubDate);
+  let pubDate = data.results[0].pubDate;
   let imageUrl = data.results[0].image_url;
   let description = data.results[0].description;
   let sourceId = data.results[0].source_id;
-  console.log(description);
-  //createAnImageNotif(title, description, pubDate, imageUrl)
-  createABasicNotif(title, description, pubDate)
-
+  console.log(imageUrl);
+  let epochDate = new Date(pubDate);
+  epochDate = epochDate.getTime();
+  console.log(pubDate);
+  console.log(epochDate);
+  //if statement if parameter(s) is unavailable
+  if (!imageUrl) {
+    if (!description) {
+      createABasicNotif(title, "Click the link to discover more!", epochDate);
+    } else {
+      createABasicNotif(title, description, epochDate);
+    }
+  } else {
+    if (!description) {
+      createAnImageNotif(
+        title,
+        "Click the link to discover more!",
+        epochDate,
+        "/images/icon.png"
+      );
+    } else {
+      createAnImageNotif(title, description, epochDate, "/images/icon.png");
+    }
+  }
+  readNow(link);
 }
-
-
-// const fetchData = fetch(urlWithTopic)
-// .then((response) => response.json())
-// .then((response) => response.results[0]);
-
-// const printTitle = () => {
-//   fetchData.then((a) => {
-//       let title = a.title;
-//       let link = a.link;
-//       let pubDate = a.pubDate;
-//       let imageUrl = a.image_url;
-//       let description = a.description;
-//       let sourceId = a.source_id;
-//       console.log(title);
-//   });
-// }
-
